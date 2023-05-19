d97faa1d78fa4cae0cf4e2a9bcf42665199badea
Mark VanLandingham <markvanlan@gmail.com>
2023-05-19 12:07:19 -0500

    grr

--- assets/javascripts/discourse/routes/preferences-github.js
+++ assets/javascripts/discourse/routes/preferences-github.js
@@ -2,7 +2,7 @@ import RestrictedUserRoute from "discourse/routes/restricted-user";
 import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class PreferencesChatRoute extends RestrictedUserRoute {
-  beforeModel(transition) {
+  beforeModel() {
     if (!this.site.github_verification_enabled) {
       return this.router.transitionTo(`discovery.${defaultHomepage()}`);
     }
